2a102a84c0da19ea7b96989196a60a423a4a89c8
dadanisme <dadanisme@upi.edu>
2022-06-06 22:58:35 +0700

    asal asalan

--- src/App.js
+++ src/App.js
@@ -8,28 +8,42 @@ function App() {
   const [response, setResponse] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isUpload, setIsUpload] = useState(false);
+  const [uploadLoading, setUploadLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getResponse = () => {
-    fetch(`http://127.0.0.1:5000/getResponse?filename=${fileUrl}`)
+    setIsLoading(true);
+    fetch(`http://cv.lskpengantin.id/getResponse?filename=${fileUrl}`)
       .then((res) => res.json())
       .then((data) => {
         setResponse(data);
-        setImgUrl(`http://127.0.0.1:5000/output/${data.filename}`);
+        setImgUrl(`http://cv.lskpengantin.id/output/${data.filename}`);
         setIsSubmitted(true);
+        setIsLoading(false);
       });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsUpload(true);
+    setUploadLoading(true);
     const formData = new FormData(e.target);
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5000/upload");
+    await xhr.open("POST", "http://cv.lskpengantin.id/upload");
     xhr.send(formData);
 
     xhr.onload = () => {
+      setIsUpload(true);
       setFileUrl(JSON.parse(xhr.responseText).filename);
+      setUploadLoading(false);
     };
+  };
+
+  const handleReset = () => {
+    setIsSubmitted(false);
+    setIsUpload(false);
+    setFileUrl("");
+    setImgUrl("");
+    setResponse({});
   };
 
   return (
@@ -51,60 +65,95 @@ function App() {
         encType="multipart/form-data"
         onSubmit={handleSubmit}
       >
-        <InputGroup>
-          <FormControl
-            name="file"
-            type="file"
-            placeholder="Recipient's username"
-            aria-label="Recipient's username with two button addons"
-          />
-          <Button type="submit" variant="outline-secondary">
-            Upload
-          </Button>
-          <Button
-            onClick={getResponse}
-            variant="outline-dark"
-            disabled={!isUpload}
-          >
-            Get Response
-          </Button>
-        </InputGroup>
+        {isUpload ? (
+          <div>
+            <Button
+              style={{
+                marginRight: "10px",
+              }}
+              onClick={getResponse}
+              variant="outline-dark"
+            >
+              Get Response
+            </Button>
+            <Button type="button" variant="outline-dark" onClick={handleReset}>
+              Try another image!
+            </Button>
+          </div>
+        ) : (
+          <InputGroup>
+            <FormControl
+              name="file"
+              type="file"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username with two button addons"
+            />
+            <Button type="submit" variant="outline-secondary">
+              Upload
+            </Button>
+          </InputGroup>
+        )}
       </form>
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <h2>Output</h2>
-        {isSubmitted ? (
-          <img style={{ maxHeight: "500px" }} src={imgUrl} alt="file" />
-        ) : (
-          ""
-        )}
+      {uploadLoading ? (
+        <div>Uploading...</div>
+      ) : (
         <div
-          className="mt-3"
           style={{
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          {isSubmitted ? (
-            <div>
-              <h3>Texts detected:</h3>
-              {response.results.map((item, index) => (
-                <div key={index}>
-                  <p style={{ color: item.color }}>{item.text} </p>
-                </div>
-              ))}
-            </div>
+          {isLoading ? (
+            <div>Loading...</div>
           ) : (
-            "No response yet"
+            <>
+              <h2>Output</h2>
+              {isUpload ? (
+                <img style={{ maxHeight: "500px" }} src={imgUrl} alt="file" />
+              ) : (
+                ""
+              )}
+              <div
+                className="mt-3"
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                {isSubmitted ? (
+                  <div>
+                    <h3>Texts detected:</h3>
+                    <p>
+                      {fileUrl} <Badge bg="warning">file url</Badge>
+                    </p>
+                    {response.results.map((item, index) => {
+                      const color =
+                        index > 0
+                          ? `rgb(${item.color[0]}, ${item.color[1]}, ${item.color[2]})`
+                          : "";
+                      return (
+                        <div key={index}>
+                          <p
+                            style={{
+                              color: color,
+                            }}
+                          >
+                            {item.text}{" "}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  "No response yet"
+                )}
+              </div>
+            </>
           )}
         </div>
-      </div>
+      )}
     </div>
   );
 }
